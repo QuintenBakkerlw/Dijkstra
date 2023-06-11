eae8cf87f13a419f12eb82f15d961e2ceda194c3
quint <quintensport@gmail.com>
2023-06-11 22:06:07 +0200

    Delete shortestpath cpp & hpp

--- src/shortestPath.hpp
+++
@@ -1,9 +0,0 @@
-// #include <iostream>
-// using namespace std;
-// class shortestPath
-// {
-// private:
-//     int distance[4], visited[4];
-// public:
-//     shortestPath(int distance[4],bool visited[4]);
-// };
